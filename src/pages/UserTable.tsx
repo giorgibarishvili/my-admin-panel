4a7a7efb0d2daa9061d684e0d5a3ba924413ae40
Giorgi Barishvili <90037916+giorgibarishvili@users.noreply.github.com>
2025-01-10 23:51:02 +0400

    create fetchusers function in usertable

--- src/pages/UserTable.tsx
+++ src/pages/UserTable.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: number;
+  role: string;
+}
 
 function UserTable() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get("https://dummyjson.com/users");
+        setUsers(res.data.users);
+      } catch (err) {
+        setError("failed to fetch data");
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
@@ -22,28 +51,30 @@ function UserTable() {
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200">
-          <tr>
-            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              John Doe
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-              24/05/1995
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-              Web Developer
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-              $120,000
-            </td>
-            <td className="whitespace-nowrap px-4 py-2">
-              <button className="inline-block rounded bg-blue-600 px-4 py-2 text-xs font-medium text-white hover:bg-blue-600 mx-2">
-                edit
-              </button>
-              <button className="inline-block rounded bg-blue-600 px-4 py-2 text-xs font-medium text-white hover:bg-blue-600 mx-2">
-                delete
-              </button>
-            </td>
-          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                {user.firstName + " " + user.lastName}
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                {user.email}
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                {user.age}
+              </td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                {user.role}
+              </td>
+              <td className="whitespace-nowrap px-4 py-2">
+                <button className="inline-block rounded bg-yellow-500 px-4 py-2 text-xs font-medium text-white hover:bg-yellow-600 mx-2">
+                  edit
+                </button>
+                <button className="inline-block rounded bg-red-500 px-4 py-2 text-xs font-medium text-white hover:bg-red-600 mx-2">
+                  delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
